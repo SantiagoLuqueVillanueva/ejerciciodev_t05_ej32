435c31bff8b2ec6b5e088521082e7b4d0208341c
Santiago Luque Villanueva <luquevillanuevasantiago@gmail.com>
2025-11-02 16:47:19 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,32 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int numeroIntroducido =Integer.parseInt(System.console().readLine("Por favor, introduzca un número entero positivo: "));
+
+        int numero = numeroIntroducido;
+        int volteado = 0;
+
+        while (numero > 0) {
+            volteado = (volteado * 10) + (numero % 10);
+            numero /= 10;
+        }
+
+        int numero2 = volteado;
+        int sol = 0;
+        int suma = 0;
+
+        while (numero2 > 0) {
+            sol = numero2 % 10;
+
+            if (sol % 2 == 0) {
+                System.out.print(sol + " ");
+                suma += sol;
+            }
+
+            numero2 /= 10;
+        }
+
+        System.out.println();
+
+        System.out.println("Suma de los digitos pares: "+suma);
     }
 }
